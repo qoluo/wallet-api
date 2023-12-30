052542b261807e78458a123833352ff50f722512
Aleksej Komnenović <dev@akom.me>
2023-12-30 17:49:41 +0100

    update tests

--- tests/routers_tests/test_records_router.py
+++ tests/routers_tests/test_records_router.py
@@ -1,10 +1,12 @@
+import pytest
+from fastapi.exceptions import RequestValidationError
 from fastapi.testclient import TestClient
 from app.routers.records import router
 
 client = TestClient(router)
 
 
-def test_direct_expense():
+def test_direct_income():
     json = {
         "type": "income",
         "account": "first",
@@ -13,6 +15,20 @@ def test_direct_expense():
         "date": "123123",
     }
 
-    response = client.post("/records/add-income", json=json)
+    response = client.post("/records/add-record", json=json)
     assert response.status_code == 201
-    assert response.json() == {"status": "Success", "type": "income"}
+    assert response.json() == {"status": "Success", "recordType": "income"}
+
+
+def test_direct_expense():
+    json = {
+        "type": "expense",
+        "account": "second",
+        "amount": 101.1,
+        "currency": "EUR",
+        "date": "22332",
+    }
+
+    response = client.post("/records/add-record", json=json)
+    assert response.status_code == 201
+    assert response.json() == {"status": "Success", "recordType": "expense"}
