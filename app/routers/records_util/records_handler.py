e92fbb07e71392b8ee5c2068c8ff26cf247bd853
Aleksej Komnenović <dev@akom.me>
2024-01-08 00:10:22 +0100

    add util for getting all records

--- app/routers/records_util/records_handler.py
+++ app/routers/records_util/records_handler.py
@@ -1,6 +1,8 @@
 import os
-from app.db.connector import get_db_client
+
 from pymongo import MongoClient
+
+from app.db.connector import get_db_client
 
 default_db_name: str = os.getenv("WALLET_DEFAULT_DB")
 records_collection_name: str = os.getenv("WALLET_RECORDS_COLLECTION")
@@ -19,3 +21,20 @@ def add_record(record: dict[str, int]):
     db_client.close()
 
     return True if result.inserted_id else False
+
+
+def get_all_records():
+    db_client: MongoClient = get_db_client()
+
+    if not db_client:
+        return False
+
+    wallet_default_db = db_client[default_db_name]
+    collection = wallet_default_db[records_collection_name]
+
+    records = collection.find({}, {"_id": 0})
+    result = [record for record in records]
+    
+    db_client.close()
+
+    return result
