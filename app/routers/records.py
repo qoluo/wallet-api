f55e033bdc7189580415a096cb767c17f84484fd
Aleksej Komnenović <dev@akom.me>
2024-01-14 17:55:04 +0100

    update naming, add delete-all endpoint

--- app/routers/records.py
+++ app/routers/records.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Query, status, Response
 from typing import Annotated
+
+from fastapi import APIRouter, Query, status, Response
 from pydantic import BaseModel
+
 from app.routers.records_util.records_handler import add_record
 from app.routers.records_util.records_handler import get_all_records
+from app.routers.records_util.records_handler import delete_all_records
 
 router = APIRouter()
 
@@ -16,7 +19,7 @@ class Record(BaseModel):
 
 
 @router.post("/records/add-record", status_code=status.HTTP_201_CREATED)
-async def expense_handler(record: Record, response: Response):
+async def add_record_handler(record: Record, response: Response):
     record_data = record.model_dump()
     insert_result: bool = add_record(record_data)
 
@@ -27,7 +30,13 @@ async def expense_handler(record: Record, response: Response):
     return {"status": "Success", "recordType": record_data["type"]}
 
 
-@router.post("/records/aggregate", status_code=status.HTTP_200_OK)
-async def aggregate_handler():
+@router.get("/records/get-all", status_code=status.HTTP_200_OK)
+async def get_all_records_handler():
     records = get_all_records()
+    return records
+
+
+@router.delete("/records/delete-all", status_code=status.HTTP_200_OK)
+async def delete_all_records_handler():
+    records = delete_all_records()
     return records
